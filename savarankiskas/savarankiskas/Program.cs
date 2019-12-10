380ea6b27c36dbaed96c49ef43570482f4dd408c
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-10 21:10:12 +0200

    spalva

---
+++ savarankiskas/savarankiskas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savarankiskas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string spalva = "";
+            while (spalva != "stop")
+            {
+                Console.WriteLine("Kokia jusu megstamiausia spalva?");
+                spalva = Console.ReadLine();
+                Console.WriteLine("mano irgi megstamiausia {0} [ENTER - testi]", spalva);
+            }
+            Console.Clear();
+            Console.WriteLine("Ate...");
+            Console.ReadLine();
+           
+        }
+    }
+}
