71e35e9c406ff05a018164f9931d566b0b06f46f
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-10 20:10:59 +0200

    1

--- Antas Pr/Antas Pr/Program.cs
+++ Antas Pr/Antas Pr/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Antas_Pr
 {
+    class Rectangle
+    {
+        //member variables
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("length: {0}", length);
+            Console.WriteLine("width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+
+    }
     class Program
+
     {
         static void Main(string[] args)
         {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
         }
     }
 }
