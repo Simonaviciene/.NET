a64a528ff99c26777cc186a534eec7caafd058ff
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-12 10:01:49 +0200

    pavyko

--- 4/4/Program.cs
+++ 4/4/Program.cs
@@ -18,8 +18,37 @@ namespace _4
         private double s8;
         private double s9;
 
+        public void PradiniaiDuomenys()
+        {
+            s1 = -6;
+            s2 = 3; 
+            s3 = 5;
+            s4 = 13;
+            s5 = 2;
+            s6 = 7;
+            s7 = 20;
+            s8 = 10;
+            s9 = -2;
 
-
+        }
+        public double MatematinisVeiksmas1()
+        {
+            return s1 + s2 * s3;
+        }
+        public double MatematinisVeiksmas2()
+        {
+            return (s4 - s5) * s6;
+        }
+        public double MatematinisVeiksmas3()
+        {
+            return (s3 + s9) + (s7 / s8);
+        }
+        public void Display()
+        {
+            Console.WriteLine("-6 + 3 * 5 = {0}", MatematinisVeiksmas1());
+            Console.WriteLine("(13 - 2) * 7 = {0}", MatematinisVeiksmas2());
+            Console.WriteLine("(5 + -2) + (20 / 10)) = {0}", MatematinisVeiksmas3());
+        }
 
 
 
@@ -27,7 +56,12 @@ namespace _4
         {
             static void Main(string[] args)
             {
+                MatematinisSkaiciavimas a = new MatematinisSkaiciavimas();
+                a.PradiniaiDuomenys();
+                a.Display();
+                Console.ReadLine();
             }
+            
         }
 
     }
