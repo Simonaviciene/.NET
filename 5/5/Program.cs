62ac38407781ec158ed4ba96fbbee07b7c33e7fa
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-12 11:39:51 +0200

    nebaigtas

--- 5/5/Program.cs
+++ 5/5/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace _5
 {
+
+    class Veiksmai
+    {
+        private double x;
+        private double y;
+
+        public double Sandauga()
+        {
+            return x * y;
+        }
+        public double Dalyba()
+        {
+            return x / y;
+        }
+        public double Suma()
+        {
+            return x + y;
+        }
+        public double Skirtumas()
+        {
+            return x - y;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Sandauga = {0}", Sandauga());
+            Console.WriteLine("Dalyba = {0}", Dalyba());
+            Console.WriteLine("Suma = {0}", Suma());
+            Console.WriteLine("Skirtumas = {0}", Skirtumas());
+        }
+
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main(double[] args)
         {
+                Veiksmai v = new Veiksmai();
+                v.Display();
+                Console.ReadLine();
+
+
+
         }
     }
+
+    }
+
 }
